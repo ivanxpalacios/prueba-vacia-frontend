0400ad9a65503b65ee3127feb1c7e0b4dec43654
Iván Palacios Martínez <ivanpalaciosdev@gmail.com>
2024-01-27 17:29:29 -0600

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+export default function App() {
+
+  // Usando buenas prácticas para el fetching de datos en React, ingresa a la siguiente API https://www.themealdb.com/api.php . Elige una categoría de comidas a tu criterio y asigna las primeras 5 comidas de la categoría que elegiste a un state.
+
+  return (
+    <>
+      <div className='max-w-[1200px] mx-auto '>
+        <h1 className='mb-10 text-[40px] text-center font-bold '>Prueba técnica para Desarrollador Frontend</h1>
+
+        {/* Muestra en pantalla una card como en el sitio de ejemplo con cada comida que almacenaste en tu arreglo, asegúrate que sea responsivo */}
+
+      </div>
+
+      {/* Con la imagen guardada en el src, crea un banner como el del ejemplo, asegurándote de que sea responsivo */}
+    </>
+
+  )
+}
